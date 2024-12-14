52e07fcad3e9ae60fe3ddb88ba415cc20a8bcc11
Abdullah Al-Karim Amrullah <abdullahalkarimamrullah01@gmail.com>
2024-12-14 23:27:36 +0800

    ✨ feat: new schema

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
         className="shrink-0 bg-border h-[1px] w-full"
       ></div>
       <footer className="container mx-auto py-6 text-muted-foreground flex justify-between gap-4 flex-wrap text-sm">
-        <p>© 2024 blackpepper-app. All rights reserved.</p>
+        <p>© 2024 wargaku-app. All rights reserved.</p>
         <div className="flex gap-2.5 items-center">
           <Link href="/terms">
             <span className="underline">Terms</span>
---
+++ src/schemas/resident.ts
@@ -0,0 +1,100 @@
+import { z } from "zod"
+
+export const getResidentByFamilySchema = z.object({
+  id: z.string({ message: "Family ID is required." }),
+})
+
+export const getDetailResidentSchema = z.object({
+  id: z.string({ message: "Resident ID is required." }),
+})
+
+export const createResidentSchema = z.object({
+  nik: z
+    .string()
+    .min(16, { message: "NIK must be at least 16 characters long." })
+    .max(16, { message: "NIK must be at most 16 characters long." }),
+  name: z.string().min(1, { message: "Name is required." }),
+  birthPlace: z.string().min(1, { message: "Birth place is required." }),
+  birthDate: z.string().date(),
+  gender: z.enum(["MALE", "FEMALE"], { message: "Gender is required." }),
+  religion: z.enum(
+    [
+      "ISLAM",
+      "KRISTEN",
+      "HINDU",
+      "BUDHA",
+      "KATOLIK",
+      "KONGHUCU",
+      "OTHERS",
+      "FEMALE",
+    ],
+    { message: "Religion is required." }
+  ),
+  education: z.enum(
+    ["SD", "SMP", "SMA_SMK", "D1", "D2", "D3", "D4", "S1", "S2", "S3"],
+    { message: "Education is required." }
+  ),
+  work: z.string().optional(),
+  marriageStatus: z.enum(
+    ["BELUM_KAWIN", "KAWIN", "CERAI_HIDUP", "CERAI_MATI"],
+    {
+      message: "Marriage status is required.",
+    }
+  ),
+  nationality: z.enum(["WNI", "WNA"], {
+    message: "Nationality is required.",
+  }),
+  phone: z.optional(
+    z
+      .string()
+      .min(10, { message: "Phone must be at least 10 characters long." })
+  ),
+})
+
+export const updateResidentSchema = z.object({
+  id: z.string({ message: "Resident ID is required." }),
+  nik: z
+    .string()
+    .min(16, { message: "NIK must be at least 16 characters long." })
+    .max(16, { message: "NIK must be at most 16 characters long." }),
+  name: z.string().min(1, { message: "Name is required." }),
+  birthPlace: z.string().min(1, { message: "Birth place is required." }),
+  birthDate: z.string().date(),
+  gender: z.enum(["MALE", "FEMALE"], { message: "Gender is required." }),
+  religion: z.enum(
+    [
+      "ISLAM",
+      "KRISTEN",
+      "HINDU",
+      "BUDHA",
+      "KATOLIK",
+      "KONGHUCU",
+      "OTHERS",
+      "FEMALE",
+    ],
+    { message: "Religion is required." }
+  ),
+  education: z.enum(
+    ["SD", "SMP", "SMA_SMK", "D1", "D2", "D3", "D4", "S1", "S2", "S3"],
+    { message: "Education is required." }
+  ),
+  work: z.string().optional(),
+  marriageStatus: z.enum(
+    ["BELUM_KAWIN", "KAWIN", "CERAI_HIDUP", "CERAI_MATI"],
+    {
+      message: "Marriage status is required.",
+    }
+  ),
+  nationality: z.enum(["WNI", "WNA"], {
+    message: "Nationality is required.",
+  }),
+  phone: z.optional(
+    z
+      .string()
+      .min(10, { message: "Phone must be at least 10 characters long." })
+  ),
+})
+
+export const deleteResidentSchema = z.object({
+  id: z.string({ message: "Resident ID is required." }),
+})
